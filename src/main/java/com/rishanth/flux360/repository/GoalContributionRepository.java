deb314864676e008f7785998b304fb8ae0302f03
Rishanth11 <rishanthsomasundaram.gmail.com>
2026-06-17 18:19:46 +0530

    goal module bug fixed

--- src/main/java/com/rishanth/flux360/repository/GoalContributionRepository.java
+++ src/main/java/com/rishanth/flux360/repository/GoalContributionRepository.java
@@ -1,6 +1,7 @@
 package com.rishanth.flux360.repository;
 
 import com.rishanth.flux360.entity.GoalContribution;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,9 +12,11 @@ import java.util.Optional;
 public interface GoalContributionRepository
         extends JpaRepository<GoalContribution, Long> {
 
+    @EntityGraph(attributePaths = {"goal"})
     List<GoalContribution>
     findByGoalIdOrderByContributionDateDesc(Long goalId);
 
+    @EntityGraph(attributePaths = {"goal"})
     Optional<GoalContribution>
     findByIdAndGoalUserId(Long contributionId,
                           Long userId);
